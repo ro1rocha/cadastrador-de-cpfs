b90962d461ff035e9035e3b4a58dadd3863cae3e
Ro Rocha <ro.rochatur@gmail.com>
2022-01-19 11:17:50 -0300

    segundo commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const pessoas = require('./pessoas.json');
+
+// Passo 1: Capturar: nome, cpf, casado, filhos do terminal
+// e guardar em variáveis de mesmos nomes
+
+// Passo 2: Criar um objeto com essas informações
+
+// Passo 3: Adicionar o objeto criado ao array pessoas
+
+// Passo 4: Salvar o array de pessoas no arquivo pessoas.json
+
+// Passo 5: Mostrar o array de pessoas no dormarto de tabela
